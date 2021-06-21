35de25a138c808ad8a352e1321d8b5f41b207e62
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-06-21 12:15:16 +0200

    added migration to enable Concrete Shore walls when Concrete walls is already researched

---
+++ migrations/00-concrete-recipe.lua
@@ -0,0 +1,9 @@
+-- if mods["SchallTankPlatoon"] then
+if game.active_mods["SchallTankPlatoon"] then
+    for index, force in pairs(game.forces) do
+        local technologies = force.technologies
+        local recipes = force.recipes
+
+        recipes["shorewall-concrete-recipe"].enabled = technologies["Schall-concrete-wall"].researched
+    end
+end
